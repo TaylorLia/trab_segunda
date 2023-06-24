755af5d2d51d5c7f7d0f7e31216277608376f501
André lucas farias <88065639+TaylorLia@users.noreply.github.com>
2023-06-23 22:55:57 -0300

    teste03

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,73 +1,81 @@
 const router = require("express").Router();
-const User = require("../models/User");
+const { PrismaClient } = require("@prisma/client");
 const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
 
-//REGISTER
-router.post("/register", async (req, res) => {
-  const newUser = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: CryptoJS.AES.encrypt(
-      req.body.password,
-      process.env.PASS_SEC
-    ).toString(),
-  });
+const prisma = new PrismaClient();
 
+// REGISTER
+router.post("/register", async (req, res) => {
+  const { username, email, password , nome, cpf, endereco, bairro, cidade, uf, cep, fone  } = req.body;
   try {
-    const savedUser = await newUser.save();
-    res.status(201).json(savedUser);
+    const newUser = await prisma.usuario.create({
+      data: {
+        USUARIO : username,
+        SENHA : CryptoJS.AES.encrypt(
+          req.body.password,
+          process.env.PASS_SEC
+        ).toString(),
+        EMAIL : email,
+        NOME : nome,
+        BAIRRO : bairro,
+        CEP : cep,
+        CIDADE : cidade,
+        UF : uf,
+        ENDERECO : endereco,
+        FONE : fone,
+        CPF : cpf
+      },
+    });
+
+    res.status(201).json(newUser);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({message : err});
+  }
+});
+
+// LOGIN
+router.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await prisma.usuario.findUnique({
+      where: {
+        USUARIO: username,
+      },
+    });
+
+    if (!user) {
+      return res.status(401).json("Wrong User Name");
+    }
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+    const inputPassword = password;
+
+    if (originalPassword !== inputPassword) {
+      return res.status(401).json("Wrong Password");
+    }
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "3d" }
+    );
+
+    const { password: _, ...others } = user;
+
+    res.status(200).json({ ...others, accessToken });
   } catch (err) {
     res.status(500).json(err);
   }
-
-  
-
-});
-
-//LOGIN
-
-router.post('/login', async (req, res) => {
-    try{
-        const user = await User.findOne(
-            {
-                userName: req.body.user_name
-            }
-        );
-
-        
-
-        !user && res.status(401).json("Wrong User Name");
-
-        const hashedPassword = CryptoJS.AES.decrypt(
-            user.password,
-            process.env.PASS_SEC
-        );
-
-
-        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
-
-        const inputPassword = req.body.password;
-        
-        originalPassword != inputPassword &&
-            res.status(401).json("Wrong Password");
-
-        const accessToken = jwt.sign(
-        {
-            id: user._id,
-            isAdmin: user.isAdmin,
-        },
-        process.env.JWT_SEC,
-            {expiresIn:"3d"}
-        );
-  
-        const { password, ...others } = user._doc;  
-        res.status(200).json({...others, accessToken});
-
-    }catch(err){
-        res.status(500).json(err);
-    }
-
 });
 
 module.exports = router;
--- api/routes/stripe.js
+++ api/routes/stripe.js
@@ -1,21 +1,33 @@
-const router = require("express").Router();
+const { PrismaClient } = require("@prisma/client");
 const stripe = require("stripe")("sk_test_51NKOC3IP0saOLRhOpP5xyHMVEag7tfJHo5cyZWEl2tCAqDVIHbNve55nAwHhZeJheCzxZ8ZPb2lXqcufW1hxAFYo003kMP3IRZ");
 
-router.post("/payment", (req, res) => {
-  stripe.charges.create(
-    {
-      source: req.body.tokenId,
-      amount: req.body.amount,
-      currency: "usd", //dolar
-    },
-    (stripeErr, stripeRes) => {
-      if (stripeErr) {
-        res.status(500).json(stripeErr);
-      } else {
-        res.status(200).json(stripeRes);
-      }
-    }
-  );
+const prisma = new PrismaClient();
+const router = require("express").Router();
+
+router.post("/payment", async (req, res) => {
+  const { tokenId, amount } = req.body;
+
+  try {
+    const stripeRes = await stripe.charges.create({
+      source: tokenId,
+      amount,
+      currency: "usd", // dollar
+    });
+
+    const payment = await prisma.payment.create({
+      data: {
+        tokenId,
+        amount,
+        currency: "usd",
+        status: stripeRes.status,
+        // Outros campos relevantes do pagamento
+      },
+    });
+
+    res.status(200).json(payment);
+  } catch (stripeErr) {
+    res.status(500).json(stripeErr);
+  }
 });
 
 module.exports = router;
