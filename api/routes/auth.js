f7c9aed74ca96a461aa90fe0ae666ff94c4e70f1
Marcio Junior <marciohamer@gmail.com>
2023-06-24 02:44:11 -0300

    implementado full login

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -52,47 +52,81 @@ try {
 });
 
 //LOGIN
-
 router.post('/login', async (req, res) => {
-    try{
-        const user = await User.findOne(
-            {
-                userName: req.body.user_name
-            }
-        );
+  try {
+    const user = await prisma.usuario.findFirst({
+      where : {
+        usuario : req.username
+      }
+    })
 
-        
+    !user && res.status(401).json("Usuario não encontrado");
 
-        !user && res.status(401).json("Wrong User Name");
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.senha,
+      process.env.PASS_SEC
+  );
+      const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+      const inputPassword = req.body.password;        
+      originalPassword != inputPassword &&
+      res.status(401).json("Senha invalida");
 
-        const hashedPassword = CryptoJS.AES.decrypt(
-            user.password,
-            process.env.PASS_SEC
-        );
-
-
-        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
-
-        const inputPassword = req.body.password;
-        
-        originalPassword != inputPassword &&
-            res.status(401).json("Wrong Password");
-
-        const accessToken = jwt.sign(
+      const accessToken = jwt.sign(
         {
-            id: user._id,
-            isAdmin: user.isAdmin,
+            id: user.id,
+            isAdmin: user.is_adm,
         },
         process.env.JWT_SEC,
             {expiresIn:"3d"}
         );
-  
-        const { password, ...others } = user._doc;  
-        res.status(200).json({...others, accessToken});
 
-    }catch(err){
-        res.status(500).json(err);
-    }
+        res.status(200).json({...user, accessToken});
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({error});
+  }  
+  
+  // try{
+
+      
+  //       const user = await User.findOne(
+  //           {
+  //               userName: req.body.user_name
+  //           }
+  //       );
+
+  //       !user && res.status(401).json("Wrong User Name");
+
+  //       const hashedPassword = CryptoJS.AES.decrypt(
+  //           user.password,
+  //           process.env.PASS_SEC
+  //       );
+
+        
+
+
+  //       const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+  //       const inputPassword = req.body.password;
+        
+  //       originalPassword != inputPassword &&
+  //           res.status(401).json("Wrong Password");
+
+  //       const accessToken = jwt.sign(
+  //       {
+  //           id: user._id,
+  //           isAdmin: user.isAdmin,
+  //       },
+  //       process.env.JWT_SEC,
+  //           {expiresIn:"3d"}
+  //       );
+  
+  //       const { password, ...others } = user._doc;  
+  //       res.status(200).json({...others, accessToken});
+
+  //   }catch(err){
+  //       res.status(500).json(err);
+  //   }
 
 });
 
